684873ae368b7d29999998f7a6274fba00a96cfb
luciolemos <luciolemos.j5@gmail.com>
2026-05-19 03:35:42 +0000

    psicologia

--- config/content/landing.php
+++ config/content/landing.php
@@ -238,8 +238,8 @@ return [
     'footer' => [
         'title' => 'Jersika Carvalho',
         'label' => 'Psicóloga • CRP 17/1671',
-        'address' => 'Atendimento psicológico para mulheres em Natal/RN',
-        'meta' => 'Psicologia Corporal • Especialização em Família • Psicologia Analítica (em formação) • Terapeuta de Mulheres (em formação)',
+        'address' => 'Instagram: @jersikacarvalho.psi • +55 71 8400-5128 • jersikacarvalho.psi@gmail.com',
+        'meta' => 'Atendimento psicológico para mulheres • Psicologia Corporal • Especialização em Família • Psicologia Analítica (em formação) • Terapeuta de Mulheres (em formação)',
         'emergency_note' => 'Atendimento eletivo. Em caso de crise aguda, risco imediato ou urgência em saúde mental, procure atendimento emergencial presencial.',
         'credit' => 'Desenvolvido por <a href="https://natalcode.com.br/" target="_blank" rel="noopener noreferrer">NatalCode</a> - Soluções Digitais',
     ],
